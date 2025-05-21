451e00d6ab4c23579ecb8d3b40b961c41bb029e7
Ninad171203 <149476576+Ninad171203@users.noreply.github.com>
2025-05-22 00:21:17 +0530

    Add files via upload

---
+++ testbed_navigation/launch/localization.launch.py
@@ -0,0 +1,57 @@
+import os
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+
+    use_sim_time_arg = DeclareLaunchArgument(
+        "use_sim_time",
+        default_value="true"
+    )
+
+    amcl_config_arg = DeclareLaunchArgument(
+        "amcl_config",
+        default_value=os.path.join(
+            get_package_share_directory("testbed_navigation"),
+            "config",
+            "amcl_params.yaml"
+        )
+    )
+
+    use_sim_time = LaunchConfiguration("use_sim_time")
+    amcl_config = LaunchConfiguration("amcl_config")
+
+    lifecycle_nodes = ["amcl"]
+
+    nav2_amcl = Node(
+        package="nav2_amcl",
+        executable="amcl",
+        name="amcl",
+        output="screen",
+        parameters=[
+            amcl_config,
+            {"use_sim_time": use_sim_time}
+        ]
+    )
+
+    nav2_lifecycle_manager = Node(
+        package="nav2_lifecycle_manager",
+        executable="lifecycle_manager",
+        name="lifecycle_manager_localization",
+        output="screen",
+        parameters=[
+            {"node_names": lifecycle_nodes},
+            {"use_sim_time": use_sim_time},
+            {"autostart": True}
+        ]
+    )
+
+    return LaunchDescription([
+        use_sim_time_arg,
+        amcl_config_arg,
+        nav2_amcl,
+        nav2_lifecycle_manager
+    ])
