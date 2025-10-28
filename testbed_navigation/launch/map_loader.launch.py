8b95d7a9c30e13521fa30239f8958bf6c6522138
Ninad171203 <149476576+Ninad171203@users.noreply.github.com>
2025-10-29 00:05:33 +0530

    Update map_loader.launch.py

--- testbed_navigation/launch/map_loader.launch.py
+++ testbed_navigation/launch/map_loader.launch.py
@@ -4,44 +4,40 @@ from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
-
 def generate_launch_description():
 
     use_sim_time_arg = DeclareLaunchArgument(
-            "use_sim_time",
-            default_value="true"
+        "use_sim_time",
+        default_value="true"
     )
 
     map_name_arg = DeclareLaunchArgument(
-            "map_name",
-            default_value="testbed_map" 
+        "map_name",
+        default_value="testbed_world"
     )
 
     map_name = LaunchConfiguration("map_name")
-    use_sim_time = LaunchConfiguration("use_sim_time") 
-
+    use_sim_time = LaunchConfiguration("use_sim_time")
     lifecycle_nodes = ["map_server"]
 
     map_path = PathJoinSubstitution([
         get_package_share_directory("testbed_bringup"),
         "maps",
-        map_name,
         "testbed_world.yaml"
     ])
 
-    Nav2_map_server = Node(
-        package = "nav2_map_server",
-        executable = "map_server",
-        output = "screen",
-        parameters = [
+    map_server = Node(
+        package="nav2_map_server",
+        executable="map_server",
+        name="map_server",
+        output="screen",
+        parameters=[
             {"yaml_filename": map_path},
-            {"use_sim_time" : use_sim_time}
-        ]    
+            {"use_sim_time": use_sim_time}
+        ]
     )
 
-    
-
-    nav2_lifecycle_manager = Node(
+    lifecycle_manager = Node(
         package="nav2_lifecycle_manager",
         executable="lifecycle_manager",
         name="lifecycle_manager_map_server",
@@ -56,6 +52,6 @@ def generate_launch_description():
     return LaunchDescription([
         use_sim_time_arg,
         map_name_arg,
-        Nav2_map_server,
-        nav2_lifecycle_manager
+        map_server,
+        lifecycle_manager
     ])
