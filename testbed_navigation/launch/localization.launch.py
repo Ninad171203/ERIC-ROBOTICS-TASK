4f6464795b018b7d98ae3578468c2b57052c0464
Ninad171203 <149476576+Ninad171203@users.noreply.github.com>
2025-10-29 07:44:58 +0530

    Update localization.launch.py

--- testbed_navigation/launch/localization.launch.py
+++ testbed_navigation/launch/localization.launch.py
@@ -1,55 +1,31 @@
 import os
-from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
 
 def generate_launch_description():
-    use_sim_time_arg = DeclareLaunchArgument(
-        "use_sim_time",
-        default_value="true"
+    nav_pkg_share = get_package_share_directory('testbed_navigation')
+    
+    amcl_node = Node(
+        package='nav2_amcl',
+        executable='amcl',
+        name='amcl',
+        output='screen',
+        parameters=[os.path.join(nav_pkg_share, 'config', 'amcl_params.yaml')]
     )
     
-    amcl_config_arg = DeclareLaunchArgument(
-        "amcl_config",
-        default_value=os.path.join(
-            get_package_share_directory("testbed_navigation"),
-            "config",
-            "amcl_params.yaml"
-        )
-    )
-
-    use_sim_time = LaunchConfiguration("use_sim_time")
-    amcl_config = LaunchConfiguration("amcl_config")
-    lifecycle_nodes = ["amcl"]
-
-    amcl = Node(
-        package="nav2_amcl",
-        executable="amcl",
-        name="amcl",
-        output="screen",
-        parameters=[
-            amcl_config,
-            {"use_sim_time": use_sim_time}
-        ]
-    )
-
     lifecycle_manager = Node(
-        package="nav2_lifecycle_manager",
-        executable="lifecycle_manager",
-        name="lifecycle_manager_localization",
-        output="screen",
+        package='nav2_lifecycle_manager',
+        executable='lifecycle_manager',
+        name='lifecycle_manager_localization',
+        output='screen',
         parameters=[
-            {"node_names": lifecycle_nodes},
-            {"use_sim_time": use_sim_time},
-            {"autostart": True}
+            {'node_names': ['amcl']},
+            {'autostart': True}
         ]
     )
-
+    
     return LaunchDescription([
-        use_sim_time_arg,
-        amcl_config_arg,
-        amcl,
+        amcl_node,
         lifecycle_manager
     ])
