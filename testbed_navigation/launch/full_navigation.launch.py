7bbebe4fc2564ef17182c4a8596f603fd043cb7f
Ninad171203 <149476576+Ninad171203@users.noreply.github.com>
2025-10-29 07:45:29 +0530

    Update full_navigation.launch.py

--- testbed_navigation/launch/full_navigation.launch.py
+++ testbed_navigation/launch/full_navigation.launch.py
@@ -1,40 +1,30 @@
 import os
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
+    nav_pkg_share = get_package_share_directory('testbed_navigation')
+    
     map_loader_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([
-            os.path.join(
-                get_package_share_directory('testbed_navigation'),
-                'launch',
-                'map_loader.launch.py'
-            )
-        ])
+        PythonLaunchDescriptionSource(
+            os.path.join(nav_pkg_share, 'launch', 'map_loader.launch.py')
+        )
     )
-
+    
     localization_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([
-            os.path.join(
-                get_package_share_directory('testbed_navigation'),
-                'launch',
-                'localization.launch.py'
-            )
-        ])
+        PythonLaunchDescriptionSource(
+            os.path.join(nav_pkg_share, 'launch', 'localization.launch.py')
+        )
     )
-
+    
     navigation_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([
-            os.path.join(
-                get_package_share_directory('testbed_navigation'),
-                'launch',
-                'navigation.launch.py'
-            )
-        ])
+        PythonLaunchDescriptionSource(
+            os.path.join(nav_pkg_share, 'launch', 'navigation.launch.py')
+        )
     )
-
+    
     return LaunchDescription([
         map_loader_launch,
         localization_launch,
