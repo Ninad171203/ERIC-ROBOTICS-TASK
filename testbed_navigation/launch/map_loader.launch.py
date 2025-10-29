dd6a1ac62c0e5bae4bcc2245a474b0528bbd04de
Ninad171203 <149476576+Ninad171203@users.noreply.github.com>
2025-10-29 07:44:32 +0530

    Update map_loader.launch.py

--- testbed_navigation/launch/map_loader.launch.py
+++ testbed_navigation/launch/map_loader.launch.py
@@ -1,57 +1,33 @@
-from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
-from launch_ros.actions import Node
+import os
 from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
 
 def generate_launch_description():
-
-    use_sim_time_arg = DeclareLaunchArgument(
-        "use_sim_time",
-        default_value="true"
+    bringup_pkg_share = get_package_share_directory('testbed_bringup')
+    
+    map_file = os.path.join(bringup_pkg_share, 'maps', 'testbed_world.yaml')
+    
+    map_server_node = Node(
+        package='nav2_map_server',
+        executable='map_server',
+        name='map_server',
+        output='screen',
+        parameters=[{'yaml_filename': map_file}]
     )
-
-    map_name_arg = DeclareLaunchArgument(
-        "map_name",
-        default_value="testbed_world"
-    )
-
-    map_name = LaunchConfiguration("map_name")
-    use_sim_time = LaunchConfiguration("use_sim_time")
-    lifecycle_nodes = ["map_server"]
-
-    map_path = PathJoinSubstitution([
-        get_package_share_directory("testbed_bringup"),
-        "maps",
-        "testbed_world.yaml"
-    ])
-
-    map_server = Node(
-        package="nav2_map_server",
-        executable="map_server",
-        name="map_server",
-        output="screen",
-        parameters=[
-            {"yaml_filename": map_path},
-            {"use_sim_time": use_sim_time}
-        ]
-    )
-
+    
     lifecycle_manager = Node(
-        package="nav2_lifecycle_manager",
-        executable="lifecycle_manager",
-        name="lifecycle_manager_map_server",
-        output="screen",
+        package='nav2_lifecycle_manager',
+        executable='lifecycle_manager',
+        name='lifecycle_manager_map',
+        output='screen',
         parameters=[
-            {"node_names": lifecycle_nodes},
-            {"use_sim_time": use_sim_time},
-            {"autostart": True}
+            {'node_names': ['map_server']},
+            {'autostart': True}
         ]
     )
-
+    
     return LaunchDescription([
-        use_sim_time_arg,
-        map_name_arg,
-        map_server,
+        map_server_node,
         lifecycle_manager
     ])
